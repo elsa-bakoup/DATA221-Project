08dd071844a85e63c190a8a262c6732bfa75188e
OghenetanoOjekudo <oghenetano.ojekudo>
2026-03-26 15:38:43 -0600

    Train Random Forest model and evaluate performance with feature importance analysis

--- random_forest_model.py
+++ random_forest_model.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 # load the dataset
 df = pd.read_csv("data/raw/Sleep_health_and_lifestyle_dataset.csv")
@@ -44,3 +47,31 @@ y = le.fit_transform(y)
 
 print("\nEncoded target counts:")
 print(pd.Series(y).value_counts())
+
+#train test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+#random forest model
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+rf.fit(X_train, y_train)
+
+#predictions + evaluation
+y_pred = rf.predict(X_test)
+
+print("\nModel Performance:")
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred, average="weighted"))
+print("Recall:", recall_score(y_test, y_pred, average="weighted"))
+print("F1 Score:", f1_score(y_test, y_pred, average="weighted"))
+
+#feature importance
+feature_importance = pd.DataFrame({
+    "Feature": X.columns,
+    "Importance": rf.feature_importances_
+}).sort_values(by="Importance", ascending=False)
+
+print("\nTop 10 Important Features:")
+print(feature_importance.head(10))
