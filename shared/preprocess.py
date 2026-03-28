6cc903f702765ca551fddf24e320a3256f5ce00d
khanhduynguyen2301-collab · <khanhduy.nguyen2301@gmail.com>
2026-03-28 00:31:08 -0600

    Split the data

--- shared/preprocess.py
+++ shared/preprocess.py
@@ -22,22 +22,21 @@ def clean_raw_data(df):
     cleaned_data["Systolic BP"] = pd.to_numeric(bp_split[0], errors="coerce")
     cleaned_data["Diastolic BP"] = pd.to_numeric(bp_split[1], errors="coerce")
 
-
+    # Drop used columns
     cleaned_health_data = cleaned_data.drop(columns=["Blood Pressure", "Person ID"])
 
     return cleaned_health_data
 
 def get_feature_columns(cleaned_health_data):
-
     numeric_cols = cleaned_health_data.select_dtypes(include=["number"]).columns.tolist()
     categorical_cols = cleaned_health_data.select_dtypes(exclude=["number"]).columns.tolist()
+
     return numeric_cols, categorical_cols
 
 def build_processor(X_train):
     numeric_cols, categorical_cols = get_feature_columns(X_train)
 
     numeric_transformer = Pipeline([("scaler", StandardScaler())])
-
     categorical_transformer = Pipeline([("onehot", OneHotEncoder(handle_unknown="ignore"))])
 
     preprocessor = ColumnTransformer([("numeric", numeric_transformer, numeric_cols),
