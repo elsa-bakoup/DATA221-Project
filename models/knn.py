6d64d857e330720f70169db90a40c463c8c54a88
annetran2602 <anne.tran2@ucalgary.ca>
2026-03-29 00:36:56 -0600

    Done KNN model

--- models/knn.py
+++ models/knn.py
@@ -1,8 +1,8 @@
 from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, roc_auc_score
 import pandas as pd
 import zipfile
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, confusion_matrix
 
 # Loading the data from the zip file
 with zipfile.ZipFile("sleep-health-and-lifestyle-dataset.zip", "r") as zip_ref:
@@ -22,3 +22,49 @@ features_train, features_test, labels_train, labels_test = train_test_split(
 
 # Stratified K-Fold
 skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+accuracyList=[]
+
+for train_index, test_index in skf.split(features_train, labels_train):
+    X_train, X_test = features_train.iloc[train_index], features_train.iloc[test_index]
+    y_train, y_test = labels_train.iloc[train_index], labels_train.iloc[test_index]
+
+    model=KNeighborsClassifier(n_neighbors=5)
+    model.fit(X_train, y_train)
+
+    predicted_result = model.predict(X_test)
+    accuracyList.append(accuracy_score(y_test, predicted_result))
+
+# Train model using full set of training data
+final_model=KNeighborsClassifier(n_neighbors=5)
+final_model.fit(features_train, labels_train)
+
+# Predict values
+predicted_result =final_model.predict(features_test)
+
+# Confusion matrix
+confusionMatrix=confusion_matrix(labels_test, predicted_result)
+labels=["None", "Insomnia", "Sleep Disorder"]
+confusionMatrix_df=pd.DataFrame(
+    confusionMatrix,
+    index=labels,
+    columns=labels)
+
+# Accuracy
+accuracy=sum(accuracyList)/len(accuracyList)
+
+# Precision
+precision=precision_score(labels_test, predicted_result, average='weighted')
+
+# recall
+recall=recall_score(labels_test, predicted_result, average='weighted')
+
+# f1score
+f1=f1_score(labels_test, predicted_result, average='weighted')
+
+# Display result
+print("Accuracy: ", accuracy)
+print("Precision: ", precision)
+print("Recall: ", recall)
+print("F1 Score: ", f1)
+print("Confusion Matrix: \n", confusionMatrix_df)
