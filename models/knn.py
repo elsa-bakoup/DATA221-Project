3c3527df20d79627709c7ad126fa5afe4e4cce8a
annetran2602 <anne.tran2@ucalgary.ca>
2026-03-29 14:06:23 -0600

    knn model completed

--- models/knn.py
+++ models/knn.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, confusion_matrix, roc_auc_score
+
 from shared.preprocess import build_processor
 from shared.preprocess import clean_raw_data
 
@@ -25,12 +27,57 @@ def split_data(cleaned_data):
     return X_train, X_test, y_train, y_test
 
 # Build knn model
-def build_knn_model(X_train, X_test, y_train, y_test):
+def build_knn_model(X_train):
     knn_model=Pipeline([
         ("preprocess_data", build_processor(X_train)),
         ("model", KNeighborsClassifier())
     ])
-    knn_model.fit(X_train, y_train)
-    predicted_result = knn_model.predict(X_test)
-    return predicted_result, y_test
+
+    return knn_model
+
+# Hyperparameter grid for tuning
+param_grid_knn={
+    "classifier__n_neighbors": [3,5,7,9,11],
+    "classifier__weights": ["uniform","distance"],
+    "classifier__metric": ["euclidean", "manhattan"]
+}
+
+X_train, X_test, y_train, y_test=split_data(load_clean_data())
+knn_model=build_knn_model(X_train)
+
+cv=StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+grid_search_knn=GridSearchCV(
+    knn_model,
+    param_grid=param_grid_knn,
+    cv=cv
+)
+
+# Train the model & prediction
+knn_model.fit(X_train, y_train)
+predicted_result=knn_model.predict(X_test)
+predicted_prob=knn_model.predict_proba(X_test)
+
+# Evaluation
+accuracy=accuracy_score(y_test, predicted_result)
+precision=precision_score(y_test, predicted_result, average='weighted')
+recall=recall_score(y_test, predicted_result, average='weighted')
+f1=f1_score(y_test, predicted_result, average='weighted')
+roc=roc_auc_score(y_test, predicted_prob, multi_class='ovr', average='weighted')
+
+# Confusion matrix
+confusionMatrix_df=confusion_matrix(y_test, predicted_result)
+labels=["None", "Insomnia", "Sleep Apnea"]
+cm_df=pd.DataFrame(
+    confusionMatrix_df,
+    index=labels,
+    columns=labels)
+
+# Display result
+print("Accuracy: ", accuracy)
+print("Precision: ", precision)
+print("Recall: ", recall)
+print("F1 Score: ", f1)
+print("ROC AUC: ", roc)
+print("Confusion Matrix: \n", cm_df)
 
